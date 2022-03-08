ac1bf2764edece5e6f16830ff97db3b9fb74eff0
Andres Lederer Dobra <andres.lederer@globant.com>
2022-03-08 16:45:52 -0300

    clean architecture set up
    clean architecture set up
    
    modules dependencies set up
    
    clean architecture set up
    
    ktlint and .gitignore added
    
    .idea dir added to .gitignore
    
    pr's comments fixes
    
    wildcards fixed
    
    .idea files added to .gitignore
    
    .gitignore files fix

--- di/src/test/java/com/example/di/ExampleUnitTest.kt
+++ di/src/test/java/com/example/di/ExampleUnitTest.kt
@@ -1,7 +1,8 @@
-package com.example.harrypotterapp
+package com.example.di
+
+import org.junit.Test
 
 import org.junit.Assert.assertEquals
-import org.junit.Test
 
 /**
  * Example local unit test, which will execute on the development machine (host).
