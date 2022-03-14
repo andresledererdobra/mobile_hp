cc2a2bcc9ace029e90a53952f04d52f0fd9f3d05
andres.lederer <andres.lederer@globant.com>
2022-03-14 10:10:49 -0300

    ktlint and syntax corrections

--- app/src/main/java/com/example/harrypotterapp/activity/SplashActivity.kt
+++ app/src/main/java/com/example/harrypotterapp/activity/SplashActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.harrypotterapp.R
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.Theme_HarryPotterApp)
 
